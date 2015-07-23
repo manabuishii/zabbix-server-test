6444a1179e5d273f892d82463cb028aa8b8a040a
Manabu ISHII <manabu.ishii.rb@gmail.com>
2015-07-23 10:18:50 +0900

    result to xml file

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -2,3 +2,7 @@ require 'serverspec'
 set :backend, :exec
 set :path, '/usr/bin:/sbin:/usr/local/sbin:$PATH'
 
+RSpec.configure do |c|
+  c.output_stream = File.open('/tmp/result/serverspec-result.xml', 'w')
+  c.formatter = 'RspecJunitFormatter'
+end
