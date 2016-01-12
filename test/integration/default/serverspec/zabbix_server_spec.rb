1760452ed581d83e63515d8179ca267268e9c337
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-01-12 16:17:34 +0900

    support test ubuntu and centos

--- test/integration/default/serverspec/zabbix_server_spec.rb
+++ test/integration/default/serverspec/zabbix_server_spec.rb
@@ -16,11 +16,14 @@ if os[:family] == 'redhat'
 end
 
 describe 'zabbix_server Daemon' do
-#  describe service(servicename) do
-#     it { should be_enabled }
-#     it { should be_running }
-#  end
-  describe command( '/usr/sbin/chkconfig | tail -n 1') do
-    its(:stdout) { should match "zabbix_server  	0:off	1:off	2:on	3:on	4:on	5:on	6:off" }
+  if os[:family] == 'redhat'
+    describe command( '/usr/sbin/chkconfig | tail -n 1') do
+      its(:stdout) { should match "zabbix_server  	0:off	1:off	2:on	3:on	4:on	5:on	6:off" }
+    end
+  else
+    describe service(servicename) do
+       it { should be_enabled }
+       it { should be_running }
+    end
   end
 end
