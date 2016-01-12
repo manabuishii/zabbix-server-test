3b62416dd975527a1ef3f776e94385df479c8a5b
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-01-12 15:21:42 +0900

    change for chkconfig daemon

--- test/integration/default/serverspec/zabbix_server_spec.rb
+++ test/integration/default/serverspec/zabbix_server_spec.rb
@@ -16,8 +16,11 @@ if os[:family] == 'redhat'
 end
 
 describe 'zabbix_server Daemon' do
-  describe service(servicename) do
-     it { should be_enabled }
-     it { should be_running }
+#  describe service(servicename) do
+#     it { should be_enabled }
+#     it { should be_running }
+#  end
+  describe command( '/usr/sbin/chkconfig | tail -n 1') do
+    its(:stdout) { should match "zabbix_server  	0:off	1:off	2:on	3:on	4:on	5:on	6:off" }
   end
 end
